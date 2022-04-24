f2efb4d557af04e76b84ad26271561f75bdc6764
Prudhvi Medapati <79889454+phanendhar@users.noreply.github.com>
2022-04-23 21:56:18 -0700

    Update Register_file_tb.v

--- Register_file_tb.v
+++ Register_file_tb.v
@@ -16,18 +16,22 @@ module reg_file_tb();
   
   initial
     begin
-      rst=0;
+     rst=0;
       RegWrite=1'b1;
       source_reg1=5'b00001;
       source_reg2=5'b00010;
-      wr_add=5'b00001;
+     wr_add=5'b00001;
       wr_data=464;
-      #9;
+      #10;
       rst=1;
-      #20;
+      wr_add=5'b10001;
+      wr_data = 433;
+      #10;
+      source_reg1=5'b10001;
+      #10;
       $finish;
     end
   
-  initial  $monitor($time"reg1=%d,reg2=%d,wr_add=%d,wr_data=%d,source1_read=%d,source2_read=%d",source_reg1,source_reg2,wr_add,wr_data,source1_read,source2_read);
+  initial  $monitor($time,"rst=%b,reg1=%d,reg2=%d,wr_add=%d,wr_data=%d,source1_read=%d,source2_read=%d",rst,source_reg1,source_reg2,wr_add,wr_data,source1_read,source2_read);
   
 endmodule
