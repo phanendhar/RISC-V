472cd9ea171f920be9331455e231807cdd7eae65
Prudhvi Medapati <79889454+phanendhar@users.noreply.github.com>
2022-04-23 21:55:41 -0700

    Update Register_file.v

--- Register_file.v
+++ Register_file.v
@@ -1,16 +1,17 @@
-module #(parameter ADDR_SIZE=5,WORD_SIZE=64) reg_file(clk,rst,RegWrite,source_reg1,source_reg2,wr_add,wr_data,source1_read,source2_read);
-  
+module reg_file #(parameter ADDR_SIZE=5,WORD_SIZE=64) (clk,rst,RegWrite,source_reg1,source_reg2,wr_add,wr_data,source1_read,source2_read);
   
   input clk,rst,RegWrite;
   input [ADDR_SIZE-1:0] source_reg1,source_reg2,wr_add;
   input [WORD_SIZE-1:0] wr_data;
   output [WORD_SIZE-1:0] source1_read,source2_read;
   
-  reg[WORD_SIZE-1:0] mem [0:(1<<ADDR_SIZE)];
+  parameter depth = 1<<ADDR_SIZE;
+  
+  reg[WORD_SIZE-1:0] mem [depth-1:0];
   
   always@(posedge clk)
     
-    if(!rst)
+   if(!rst)
       for(int i=0; i<(1<<ADDR_SIZE);i=i+1)
         begin
           mem[i] <= 0;
@@ -24,7 +25,7 @@ module #(parameter ADDR_SIZE=5,WORD_SIZE=64) reg_file(clk,rst,RegWrite,source_re
     end
   
   assign source1_read = mem[source_reg1];
-  assign source1_read = mem[source_reg2];
+  assign source2_read = mem[source_reg2];
   
 endmodule
   
